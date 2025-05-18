5b65639729da2acce0e23e3064b8aa23691bbad5
Ayşe Çeliker <158210372+1903579@users.noreply.github.com>
2025-05-18 17:58:04 +0300

    Create baslangıc.c
    Başlangıç seviyesinde yazdırma ve fonksiyonu anlama.

---
+++ baslangıc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>   //include, dahil et demektir.  stdio.h ise başlık dosyasıdır.
+int main() {    //ana fonksiyondur.
+
+	//burası yorum satırıdır. Kullanıcı tarafından görülmez.
+	printf("Ogrenciler\n"); //printf yazdırma komutudur. \n ise alt satıra geçmesini sağlar.
+	printf("***********\n");
+	printf("Ogrenci1:\n");
+	printf("Adi: Ayse\n");
+	printf("Soyadi: Celiker\n");
+	printf("Meslek: Programci");
+
+		return 0;   // return, değer döndürme demektir. return 0 dediği için 0 değeri döner.
+}
