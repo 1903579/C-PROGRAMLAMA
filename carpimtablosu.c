50672c41d8b6ae08e6c3000041693c76d6de34a5
Ayşe Çeliker <158210372+1903579@users.noreply.github.com>
2025-05-20 19:55:34 +0300

    Create carpimtablosu.c
    Çarpım Tablosu

---
+++ carpimtablosu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    
+    int i,j;
+    printf("Çoklu carpim tablosu:\n");
+    for(i=0; i<=10; i++){
+        for(j=0; j<=10; j++){
+            printf("%d\n", i*j);
+        }
+    printf("\n");
+    }
+    
+    
+    return 0;
+}
